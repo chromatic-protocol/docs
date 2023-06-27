79c0fc40aadc3219abd08916ee94eb0187b138da
dia.nn <dia.nn@quarkonix.com>
2023-06-27 17:15:05 +0900

    Set darkMode, corePlugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: ['class', '[data-theme="dark"]'],
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [],
+  corePlugins: { preflight: false }
 }

8f5cbd167f6bd58970b78ac42ecec2a90d7dfd40
dia.nn <dia.nn@quarkonix.com>
2023-06-28 09:42:33 +0900

    style: Set tailwind.config, import font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,61 @@
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: ['class', '[data-theme="dark"]'],
-  theme: {
-    extend: {}
-  },
-  plugins: [],
-  corePlugins: { preflight: false }
+const defaultTheme = require('tailwindcss/defaultTheme')
+const monospace = (function () {
+  const fonts = defaultTheme.fontFamily.mono
+  const monospace = fonts.find((font) => font === 'monospace')
+  if (monospace === null || monospace === undefined) {
+    throw new Error('no monospace')
+  }
+  return monospace
+})()
+
+export const content = ['./src/**/*.{js,jsx,ts,tsx}']
+export const darkMode = ['class', '[data-theme="dark"]']
+export const theme = {
+  extend: {
+    fontFamily: {
+      mono: ['Source Code Pro', monospace]
+    },
+    colors: {
+      current: 'currentColor',
+      black: '#030303',
+      gray: '#D4D4D4',
+      grayL: '#EEEEEE',
+      grayD: '#4D4D4D',
+
+      standby: '#FF8A00',
+      completed: '#00E23F',
+      inprocess: '#08E3D6',
+
+      long: '#a6d85b',
+      short: '#fb6115'
+    },
+    boxShadow: {
+      md: '4px 4px 20px rgba(121, 121, 121, 0.08)',
+      lg: '4px 4px 32px rgba(229, 229, 229, 0.18)',
+      xl: '4px 4px 60px rgba(229, 229, 229, 0.16)'
+    },
+    fontSize: {
+      xs: ['10px', '12px'],
+      sm: ['11px', '14px'],
+      base: ['13px', '16px'],
+      lg: ['16px', '20px'],
+      xl: ['20px', '24px'],
+      '2xl': ['24px', '28px'],
+      '3xl': ['28px', '34px'],
+      '4xl': ['36px', '40px']
+    },
+    fontWeight: {
+      light: 200,
+      normal: 300,
+      medium: 400,
+      semibold: 500,
+      bold: 600,
+      extrabold: 700
+    },
+    animation: {
+      'spin-slow': 'spin 3s linear infinite'
+    }
+  }
 }
+export const plugins = []
+export const corePlugins = { preflight: false }

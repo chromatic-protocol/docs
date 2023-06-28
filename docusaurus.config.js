fca6895638c376d74e9f311a5ef25a2cc100d11d
dia.nn <dia.nn@quarkonix.com>
2023-06-28 14:54:06 +0900

    Update Logo images

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,13 +79,13 @@ const config = {
         disableSwitch: true
       },
       navbar: {
-        title: 'CHROMATIC',
+        // title: 'CHROMATIC',
         logo: {
           alt: 'Site Logo',
-          src: 'img/chromatic-logo.svg',
-          srcDark: 'img/chromatic-logo-white.svg',
-          width: 32,
-          height: 32
+          src: 'img/chromatic-logo-full-row.svg',
+          srcDark: 'img/chromatic-logo-full-row-white.svg',
+          width: 200,
+          height: 'auto'
         },
         items: [
           {
@@ -146,6 +146,7 @@ const config = {
           {
             href: 'https://github.com/facebook/chromatic-protocol',
             label: 'Launch App',
+            class: 'btn btn-black btn-lg',
             position: 'right'
           }
         ]

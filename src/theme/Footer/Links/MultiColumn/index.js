3272e8c1e903ff9e85c127936aee1c436086a2fc
dia.nn <dia.nn@quarkonix.com>
2023-07-04 15:56:12 +0900

    style: Footer mobile

--- src/theme/Footer/Links/MultiColumn/index.js
+++ src/theme/Footer/Links/MultiColumn/index.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import LinkItem from '@theme/Footer/LinkItem';
-function ColumnLinkItem({item}) {
+import React from 'react'
+import LinkItem from '@theme/Footer/LinkItem'
+function ColumnLinkItem({ item }) {
   return item.html ? (
     <li
-      className="footer__item"
+      className="footer-item"
       // Developer provided the HTML, so assume it's safe.
       // eslint-disable-next-line react/no-danger
-      dangerouslySetInnerHTML={{__html: item.html}}
+      dangerouslySetInnerHTML={{ __html: item.html }}
     />
   ) : (
-    <li key={item.href ?? item.to} className="footer__item">
+    <li key={item.href ?? item.to} className="footer-item">
       <LinkItem item={item} />
     </li>
-  );
+  )
 }
-function Column({column}) {
+function Column({ column }) {
   return (
-    <div className="col footer__col">
-      <div className="footer__title">{column.title}</div>
-      <ul className="footer__items clean-list">
+    <div className="footer-group">
+      <div className="footer-title">{column.title}</div>
+      <ul className="footer-items clean-list">
         {column.items.map((item, i) => (
           <ColumnLinkItem key={i} item={item} />
         ))}
       </ul>
     </div>
-  );
+  )
 }
-export default function FooterLinksMultiColumn({columns}) {
+export default function FooterLinksMultiColumn({ columns }) {
   return (
-    <div className="row footer__links">
+    <div className="footer-links">
       {columns.map((column, i) => (
         <Column key={i} column={column} />
       ))}
     </div>
-  );
+  )
 }

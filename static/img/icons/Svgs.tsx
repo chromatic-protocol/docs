4d1aa7b24d6b496e91e666d7c08f5ec0897419a7
dia.nn <dia.nn@quarkonix.com>
2023-07-06 12:29:32 +0900

    wip: Add community menu

--- static/img/icons/Svgs.tsx
+++ static/img/icons/Svgs.tsx
@@ -3,6 +3,8 @@ export const Svgs = {
   contract: require('@site/static/img/icons/contract.svg').default,
   sdk: require('@site/static/img/icons/sdk.svg').default,
   twitter: require('@site/static/img/icons/twitter.svg').default,
+  discord: require('@site/static/img/icons/discord.svg').default,
+  telegram: require('@site/static/img/icons/telegram.svg').default,
   gitbook: require('@site/static/img/icons/gitbook.svg').default,
   medium: require('@site/static/img/icons/medium.svg').default,
   chromatic: require('@site/static/img/icons/chromatic.svg').default,

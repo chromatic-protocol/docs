1a84566a56ecd9bb3cb0f44a58e8e2f12fbb7fc1
dia.nn <dia.nn@quarkonix.com>
2023-11-21 14:18:29 +0900

    feat: add discord, zealy link - in Header, Footer

--- static/img/icons/Svgs.tsx
+++ static/img/icons/Svgs.tsx
@@ -4,6 +4,7 @@ export const Svgs = {
   sdk: require('@site/static/img/icons/sdk.svg').default,
   twitter: require('@site/static/img/icons/twitter.svg').default,
   discord: require('@site/static/img/icons/discord.svg').default,
+  zealy: require('@site/static/img/icons/zealy.svg').default,
   telegram: require('@site/static/img/icons/telegram.svg').default,
   gitbook: require('@site/static/img/icons/gitbook.svg').default,
   medium: require('@site/static/img/icons/medium.svg').default,

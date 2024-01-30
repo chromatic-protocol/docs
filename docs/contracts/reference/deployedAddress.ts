d99c546fb87bb80abf6ab7524962db3978ad6457
not-thing <not-thing@users.noreply.github.com>
2024-01-30 07:24:55 +0000

    Changes from contracts

--- docs/contracts/reference/deployedAddress.ts
+++ docs/contracts/reference/deployedAddress.ts
@@ -22,13 +22,16 @@ interface DeployedAddress {
  */
 export const deployedAddress: DeployedAddress = {
   "anvil": {
-    "ChromaticMarketFactory": "0x7bc06c482DEAd17c0e297aFbC32f6e63d3846650"
+    "ChromaticLens": "0x4C4a2f8c81640e47606d3fd77B353E87Ba015584",
+    "ChromaticMarketFactory": "0x7bc06c482DEAd17c0e297aFbC32f6e63d3846650",
+    "ChromaticRouter": "0x04C89607413713Ec9775E14b954286519d836FEf",
+    "ChromaticVault": "0x162A433068F51e18b7d13932F27e66a3f99E6890"
   },
   "arbitrum_sepolia": {
-    "ChromaticLens": "0xBA758C74BB48789F531342aB7d4AeED20F921B2b",
-    "ChromaticMarketFactory": "0xa74B572cA410b79B581C24cc37743d65b2D936c8",
-    "ChromaticRouter": "0x4e1F1Ae08217A7f7e3D935E1063C6d87ADA5c9Eb",
-    "ChromaticVault": "0xa18E637B138d7e25cCe6878Cfec80aE5ed44eFf9",
+    "ChromaticLens": "0xe199aaa7EF739C6f76899EAf5c06d254d34e92a1",
+    "ChromaticMarketFactory": "0x7e8e54FD9A35B3Bf908dD56Bb18928226d70810a",
+    "ChromaticRouter": "0xCEBBec615a0a3997A9Ee6a9b693524486D9180af",
+    "ChromaticVault": "0xEdA9b65D119694a7bCf3df6282541A65b7B65c3e",
     "cBTC": "0x3d44205cD54CC64C02c0Ad4C86cE53b0E871d54D",
     "cETH": "0x93252009E644138b906aE1a28792229E577239B9"
   }
